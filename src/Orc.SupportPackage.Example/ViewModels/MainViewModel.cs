53c6d4b9280673c7bf09a81305f662fcf5f2a987
Maxim Khomutov <mkhomutov@gmail.com>
2015-01-23 16:06:38 +0300

    ORCOMP-4: Added demo for getting system info

--- src/Orc.SupportPackage.Example/ViewModels/MainViewModel.cs
+++ src/Orc.SupportPackage.Example/ViewModels/MainViewModel.cs
@@ -10,14 +10,17 @@ namespace Orc.SupportPackage.Example.ViewModels
     using System;
     using System.Drawing.Imaging;
     using System.IO;
+    using System.Linq;
     using System.Threading.Tasks;
     using System.Windows;
     using System.Windows.Media.Imaging;
 
+    using Catel;
     using Catel.IoC;
     using Catel.MVVM;
 
     using Orc.SupportPackage.Services;
+    using Orc.SystemInfo.Services;
 
     public class MainViewModel : ViewModelBase
     {
@@ -26,15 +29,31 @@ namespace Orc.SupportPackage.Example.ViewModels
 
         private readonly IScreenCaptureService _screenCaptureService;
 
+        private readonly ISystemInfoService _systemInfoService;
+
         public MainViewModel(IServiceLocator serviceLocator)
         {
+            Argument.IsNotNull(() => serviceLocator);
+
             _screenCaptureService = serviceLocator.ResolveType<IScreenCaptureService>();
+            _systemInfoService = serviceLocator.ResolveType<ISystemInfoService>();
             Screenshot = new TaskCommand(OnScreenshotExecute);
+            ShowSystemInfo = new TaskCommand(OnShowSystemInfoExecute);
         }
 
         public TaskCommand Screenshot { get; private set; }
 
-        public BitmapImage ScreenPic { get; set; }
+        public TaskCommand ShowSystemInfo { get; private set; }
+
+        public BitmapImage ScreenPic { get; private set; }
+
+        public string SystemInfo { get; set; }
+
+        private async Task OnShowSystemInfoExecute()
+        {
+            var sysInfoLines = _systemInfoService.GetSystemInfo().Select(x => string.Format("{0} {1}", x.Key, x.Value));
+            SystemInfo = String.Join("\n", sysInfoLines);
+        }
 
         private async Task OnScreenshotExecute()
         {
@@ -50,7 +69,7 @@ namespace Orc.SupportPackage.Example.ViewModels
             ScreenPic.BeginInit();
             ScreenPic.CacheOption = BitmapCacheOption.OnLoad;
             ScreenPic.UriSource = new Uri(filename);
-            ScreenPic.EndInit();           
+            ScreenPic.EndInit();
         }
     }
 }
