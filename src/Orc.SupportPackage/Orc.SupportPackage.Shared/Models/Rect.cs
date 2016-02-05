29d42ae5801eaf1644f6d2a4f624da0564f3cba5
Geert van Horrik <geert@catenalogic.com>
2016-02-05 18:19:40 +0100

    Solution maintenance

--- src/Orc.SupportPackage/Orc.SupportPackage.Shared/Models/Rect.cs
+++ src/Orc.SupportPackage/Orc.SupportPackage.Shared/Models/Rect.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="Rect.cs" company="Wild Gums">
-//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// <copyright file="Rect.cs" company="WildGums">
+//   Copyright (c) 2008 - 2015 WildGums. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
