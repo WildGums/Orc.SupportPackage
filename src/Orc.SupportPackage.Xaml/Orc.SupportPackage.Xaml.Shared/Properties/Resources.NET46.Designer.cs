0a5bee25267a2c814fe652ef4ddcc0fd78d711bc
Geert van Horrik <geert@catenalogic.com>
2017-05-31 13:50:35 +0200

    Fix default namespace for Orc.SupportPackage.Xaml (NET 4.6)

--- src/Orc.SupportPackage.Xaml/Orc.SupportPackage.Xaml.Shared/Properties/Resources.NET46.Designer.cs
+++ src/Orc.SupportPackage.Xaml/Orc.SupportPackage.Xaml.Shared/Properties/Resources.NET46.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Orc.SupportPackage.Xaml.Properties {
+namespace Orc.SupportPackage.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Orc.SupportPackage.Xaml.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Orc.SupportPackage.Xaml.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Orc.SupportPackage.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
