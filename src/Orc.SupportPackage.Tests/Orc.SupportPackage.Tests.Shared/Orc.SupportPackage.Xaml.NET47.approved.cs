a5c8f62e33206f896793c4d085c66defb6ddfa98
Geert van Horrik <geert@catenalogic.com>
2018-03-07 12:59:28 +0100

    Approve API changes

--- src/Orc.SupportPackage.Tests/Orc.SupportPackage.Tests.Shared/Orc.SupportPackage.Xaml.NET47.approved.cs
+++ src/Orc.SupportPackage.Tests/Orc.SupportPackage.Tests.Shared/Orc.SupportPackage.Xaml.NET47.approved.cs
@@ -12,15 +12,29 @@ public class static ModuleInitializer
 namespace Orc.SupportPackage
 {
     
+    public class CustomPathsPackageFileSystemArtifact : Orc.SupportPackage.SupportPackageFileSystemArtifact
+    {
+        public CustomPathsPackageFileSystemArtifact(string title, System.Collections.Generic.List<string> paths, bool includeInSupportPackage) { }
+        public System.Collections.ObjectModel.ReadOnlyCollection<string> Paths { get; }
+    }
     public sealed class DefaultSupportPackageContentProvider : Orc.SupportPackage.ISupportPackageContentProvider
     {
         public DefaultSupportPackageContentProvider(Catel.Services.ILanguageService languageService) { }
         [System.Runtime.CompilerServices.IteratorStateMachineAttribute(typeof(Orc.SupportPackage.DefaultSupportPackageContentProvider.<GetSupportPackageFileSystemArtifacts>d__2))]
         public System.Collections.Generic.IEnumerable<Orc.SupportPackage.SupportPackageFileSystemArtifact> GetSupportPackageFileSystemArtifacts() { }
     }
+    public interface ISupportPackageBuilderService
+    {
+        System.Threading.Tasks.Task<bool> CreateSupportPackageAsync(string fileName, System.Collections.Generic.List<Orc.SupportPackage.SupportPackageFileSystemArtifact> artifacts);
+    }
     public interface ISupportPackageContentProvider
     {
         System.Collections.Generic.IEnumerable<Orc.SupportPackage.SupportPackageFileSystemArtifact> GetSupportPackageFileSystemArtifacts();
+    }
+    public class SupportPackageBuilderService : Orc.SupportPackage.ISupportPackageBuilderService
+    {
+        public SupportPackageBuilderService(Orc.SupportPackage.ISupportPackageService supportPackageService, Orc.FileSystem.IFileService fileService) { }
+        public System.Threading.Tasks.Task<bool> CreateSupportPackageAsync(string fileName, System.Collections.Generic.List<Orc.SupportPackage.SupportPackageFileSystemArtifact> artifacts) { }
     }
     public class SupportPackageDirectory : Orc.SupportPackage.SupportPackageFileSystemArtifact
     {
@@ -32,10 +46,8 @@ namespace Orc.SupportPackage
         public SupportPackageFileNamePattern(string title, string[] fileNamePatterns, bool includeInSupportPackage = True) { }
         public string[] FileNamePatterns { get; }
     }
-    public class SupportPackageFileSystemArtifact : Catel.Data.ModelBase
+    public class SupportPackageFileSystemArtifact
     {
-        public static readonly Catel.Data.PropertyData IncludeInSupportPackageProperty;
-        public static readonly Catel.Data.PropertyData TitleProperty;
         protected SupportPackageFileSystemArtifact(string title, bool includeInSupportPackage) { }
         public bool IncludeInSupportPackage { get; set; }
         public string Title { get; set; }
@@ -46,11 +58,19 @@ namespace Orc.SupportPackage.ViewModels
     
     public class SupportPackageViewModel : Catel.MVVM.ViewModelBase
     {
+        public static readonly Catel.Data.PropertyData IncludeCustomPathsInSupportPackageProperty;
         public static readonly Catel.Data.PropertyData LastSupportPackageFileNameProperty;
-        public SupportPackageViewModel(Catel.Services.ISaveFileService saveFileService, Orc.SupportPackage.ISupportPackageService supportPackageService, Catel.Services.IPleaseWaitService pleaseWaitService, Catel.Services.IProcessService processService, Catel.Services.ILanguageService languageService, Catel.IoC.IServiceLocator serviceLocator) { }
+        public SupportPackageViewModel(Catel.Services.ISaveFileService saveFileService, Orc.SupportPackage.ISupportPackageBuilderService supportPackageService, Catel.Services.IPleaseWaitService pleaseWaitService, Catel.Services.IProcessService processService, Catel.Services.ISelectDirectoryService selectDirectoryService, Catel.Services.IOpenFileService openFileService, Catel.Services.ILanguageService languageService, Catel.IoC.IServiceLocator serviceLocator) { }
+        public Catel.MVVM.TaskCommand AddDirectoryCommand { get; set; }
+        public Catel.MVVM.TaskCommand AddFileCommand { get; }
         public Catel.MVVM.TaskCommand CreateSupportPackage { get; }
+        public Catel.Collections.FastObservableCollection<string> CustomPaths { get; }
+        public bool IncludeCustomPathsInSupportPackage { get; set; }
         public string LastSupportPackageFileName { get; }
         public Catel.MVVM.Command OpenDirectory { get; }
+        public Catel.MVVM.Command RemovePathCommand { get; }
+        public System.Collections.Generic.List<string> SelectedCustomPaths { get; }
+        public Catel.MVVM.Command<System.Windows.Controls.SelectionChangedEventArgs> SelectionChangedCommand { get; }
         public System.Collections.Generic.List<Orc.SupportPackage.SupportPackageFileSystemArtifact> SupportPackageFileSystemArtifacts { get; }
     }
 }
