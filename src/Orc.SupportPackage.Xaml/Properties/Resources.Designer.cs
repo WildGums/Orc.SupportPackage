a7ef4c013aeae79c3a772a72953ebbabead78c21
Artem Saikin <art.saikin@gmail.com>
2021-05-07 16:14:08 +0300

    Add close button

--- src/Orc.SupportPackage.Xaml/Properties/Resources.Designer.cs
+++ src/Orc.SupportPackage.Xaml/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Orc.SupportPackage.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -75,6 +75,15 @@ namespace Orc.SupportPackage.Properties {
         internal static string SupportPackage_AddFile {
             get {
                 return ResourceManager.GetString("SupportPackage_AddFile", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Close.
+        /// </summary>
+        internal static string SupportPackage_Close {
+            get {
+                return ResourceManager.GetString("SupportPackage_Close", resourceCulture);
             }
         }
         
