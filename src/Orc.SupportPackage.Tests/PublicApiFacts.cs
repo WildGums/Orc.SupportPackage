77fda54d5292b19eef01fcceebb0669ac2321ee9
Geert van Horrik <geert@catenalogic.com>
2023-01-12 19:53:52 +0100

    Fix analyzers

--- src/Orc.SupportPackage.Tests/PublicApiFacts.cs
+++ src/Orc.SupportPackage.Tests/PublicApiFacts.cs
@@ -1,11 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="PublicApiFacts.cs" company="WildGums">
-//   Copyright (c) 2008 - 2017 WildGums. All rights reserved.
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
-
-
-namespace Orc.SupportPackage.Tests
+﻿namespace Orc.SupportPackage.Tests
 {
     using System.Reflection;
     using System.Runtime.CompilerServices;
